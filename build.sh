832c1c51ea4b0aa90fdb519e5b701298d4d12784
Alexmontesino96 <alexmontesino96@icloud.com>
2026-05-28 10:52:09 -0400

    Fix Render build: use flexible versions + --prefer-binary for pandas/asyncpg

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+# Prefer binary wheels to avoid compiling from source
+pip install --prefer-binary -r requirements.txt
 
 # Run migrations against Supabase PostgreSQL
 alembic upgrade head
